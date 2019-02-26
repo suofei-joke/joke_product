4b46aa91564a01b60f8408c042e013586472a74b
lijiaxu <lijiaxu@peopleyun.cn>
2019-02-26 15:23:09 +0800

    m

--- console/controllers/ArticleController.php
+++ console/controllers/ArticleController.php
@@ -9,11 +9,23 @@
 namespace console\controllers;
 
 
+use common\models\Gather;
 use yii\console\Controller;
 use yii\web\NotFoundHttpException;
 
 class ArticleController extends Controller
 {
+    public function actionTest()
+    {
+        $gather = new Gather();
+        $gather->name = 'a';
+        $gather->category = 'b';
+        $gather->url = 'c';
+        $gather->url_org = 'd';
+        $gather->res = 0;
+        $gather->result = 'e';
+        $gather->save();
+    }
     public function actionRun($name)
     {
         $className = '\console\models\\'.ucfirst(strtolower($name)) . 'Spider';
