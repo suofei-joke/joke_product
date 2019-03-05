c4b205cfa8aecf5678b31f8d0cae15d916f6f221
lijiaxu <lijiaxu@peopleyun.cn>
2019-03-05 15:04:32 +0800

    m

--- console/models/Jokeji_gaoxiaoSpider.php
+++ console/models/Jokeji_gaoxiaoSpider.php
@@ -11,5 +11,23 @@ namespace console\models;
 
 class Jokeji_gaoxiaoSpider extends ArticleSpider
 {
+    private $_url;
 
+    public function __construct()
+    {
+        $this->name = 'jokeji';
+        $this->baseUrl = 'http://www.jokeji.cn';
+        $this->category = 'http://www.jokeji.cn/Keyword.htm';
+    }
+
+    public function process()
+    {
+        $pages = $this->getPages($this->category);
+        if ($pages) {
+            foreach ($pages as $p) {
+                $this->urls($p);
+                die;
+            }
+        }
+    }
 }
